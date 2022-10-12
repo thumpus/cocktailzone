2b7779db396fa72abf3872831bb1032427e290bb
thumpus <elliotjwallis@gmail.com>
2022-10-12 13:46:20 -0400

    lolol

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from forms import RegisterForm, LoginForm
 import requests, os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://flask-heroku')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.getenv('DATABASE_URL', 'postgresql://flask-heroku')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 
